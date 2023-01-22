c3b70fe7ce91ee453c3e78eab696157f0500fccd
Jim Kang <jimkang@gmail.com>
2023-01-22 10:30:37 -0500

    Get rid of previous loaded svg when rendering new one.

--- renderers/render-svg.js
+++ renderers/render-svg.js
@@ -1,8 +1,10 @@
 import { getPathsFromSVG } from '../util/svg-utils';
+import { select } from 'd3-selection';
 
 var svgContentsNode = document.getElementById('svg-contents');
 
 export function renderSVG({ root }) {
+  select(svgContentsNode).selectAll('path').remove();
   var paths = getPathsFromSVG({ svgNode: root, discardTransforms: true });
   // cloneNode is necessary because appending it here will remove it from its
   // source tree.
